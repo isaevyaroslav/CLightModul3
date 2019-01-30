8a3217554fc414dc47d91b3173e499b09c6ff265
Ярослав Исаев <iyaroslav@yandex.ru>
2019-01-30 22:07:26 +0300

    Task2

--- CLightModul3/Task1.cs
+++ CLightModul3/Task1.cs
@@ -10,52 +10,97 @@ namespace CLightModul3
     {
         static void Main(string[] args)
         {
-            int[,] table = { { 2, 3, 3 }, { 3, 4, 5 } };
-            int[,] order = { { 0, 1, 2 }, { 0, 0, 0 } };
+            int[,] tables = { { 2, 3 }, { 3, 4 }, { 3, 5 } , { 4, 7 } , { 3, 6 } , { 3, 6 } };
+            int[] order = new int[tables.GetLength(0)] ;
             
 
             string greeting = 
                 "Добро пожаловать в ресторан.\n " +
                 "У нас Вы можете забронировать места зараннее.";
             string freePlaces = "У нас остались свободные места за следующими столами: ";
-            string tableTask = "Введите номер стола за которым хотите сидеть: ";
+            string tableTask = "Введите номер стола за которым хотите сидеть или Exit, если хотите выйти: ";
+            string wrongTable = "Такого стола нет";
+            string wrongPlaces = "За столом {0} не хватает мест, выберите другой стол.";
+            string tableRezerved = "Этот стол занят.";
             string placeTask = "Введите кличество мест, которое хотите занять: ";
+            string rezultTitle = "Вы забронировали места: ";
+            string rezultFormat = "Стол {0} - {1} мест.";
+            string bye = "Пока";
 
 
 
             bool exit = false;
 
-            string commandName = "";
+            string commandName = "Bron";
+
+            Console.WriteLine(greeting);
 
             while (!exit)
             {
-                Console.Clear();
-
-                for (int i = 0; i < table.Length; i++)
+                switch (commandName)
                 {
-                    if (table[i,0] > 0) ;
-                    Console.WriteLine("Стол №" + i + " - " + table[i,0] + ", занято "+ (table[i, 1]- table[i, 0]));
+                    case "Exit":
+                        exit = true;
+                        Console.WriteLine(bye);
+                        continue;
+                    case "Bron":
+                        Console.WriteLine(freePlaces);
+                        for (int i = 0; i < tables.GetLength(0); i++)
+                        {
+                            Console.WriteLine(
+                                "Стол №" + i + ": свобно - " 
+                                + tables[i, 0] + ", занято "
+                                + (tables[i, 1] - tables[i, 0]) + " мест."
+                                );
+                        }
+                        Console.Write(tableTask);
+                        string currentCommand = Console.ReadLine().ToLower();
+                        if(currentCommand == "exit")
+                        {
+                            commandName = "Exit";
+                            continue;
+                        }
+                        int currentTable = Convert.ToInt32(currentCommand);
+
+                        if (currentTable >= 0 && currentTable < tables.GetLength(0) )
+                        {
+                            if(tables[currentTable, 1] == tables[currentTable, 0])
+                            {
+                                Console.WriteLine(tableRezerved);
+                                continue;
+                            }
+                            Console.Write(placeTask);
+                            int currentPlaceCount = Convert.ToInt32(Console.ReadLine());
+                            if(currentPlaceCount > tables[currentTable, 0])
+                            {
+
+                                Console.WriteLine(wrongPlaces);
+                                commandName = "Bron";
+                                continue;
+                            }
+                            else
+                            {
+                                order[currentTable] = currentPlaceCount;
+                                tables[currentTable, 0] -= currentPlaceCount;
+                                Console.WriteLine(rezultTitle);
+                                for (int tableNumber = 0; tableNumber < order.Length; tableNumber++)
+                                {
+                                    if(order[tableNumber] > 0)
+                                    {
+                                        Console.WriteLine(rezultFormat, tableNumber, order[tableNumber]);
+                                    }
+                                }
+                                commandName = "Bron";
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(wrongTable);
+                            continue;
+                        }
+                            
+                        break;
                 }
-
-                Console.Write("\nЗа каким столом вы хотите бронь?: ");
-                int bron = Convert.ToInt32(Console.ReadLine());
-                if (table[bron] == 0)
-                {
-                    Console.WriteLine("Мест нет(");
-                    continue;
-                }
-
-                Console.Write("Сколько мест вам нужно?: ");
-                int place = Convert.ToInt32(Console.ReadLine());
-
-                if (table[bron] >= place && place > 0)
-                {
-                    table[bron] -= place;
-                    Console.WriteLine(place + " места за " + bron + " столом зарезервированы.");
-                }
-                else { Console.WriteLine("Недостаточно мест."); }
-
-                Console.ReadKey();
             }
         }
     }
---
+++ CLightModul3/Task2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLightModul3
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            int windowWidth = 94;
+            int windowHeight = 50;
+            int mapWidth = 40; 
+            int mapHeight = 40;
+            char mushroom = 'T';
+            char mushroomCeeperRight = 'R';
+            char mushroomCeeperLeft = 'Я';
+            char mushroomCeeper = mushroomCeeperRight;
+            int mushroomCounter = 0;
+            Random random = new Random();
+
+            Console.SetWindowSize(Console.WindowWidth, windowWidth);
+            Console.SetWindowSize(Console.WindowHeight, windowHeight);
+
+
+            char[,] map = new char[mapWidth, mapHeight];
+            for (int row = 0; row < mapHeight; row++)
+            {
+                for (int col = 0; col < mapWidth; col++)
+                {
+                    if (row == 0 || col == 0 || row == mapHeight-1 || col == mapWidth-1)
+                    {
+                        map[row, col] = '#';
+                    }
+                    else
+                    {
+                        map[row, col] = ' ';
+                        if(row % (random.Next(mapHeight)+1) == 0 && col% (random.Next(mapWidth)+1) == 0)
+                        {
+                            map[row, col] = mushroom;
+                            mushroomCounter++;
+                        }
+                    }
+                }
+            }
+            char[] bag = new char[0];
+
+
+            int Y = 1, X = 1;
+            while (true)
+            {
+                Console.SetCursorPosition(mapWidth+5, 0);
+                Console.Write("Здравствуйте, Вы пришли в лес за грибами.");
+                Console.SetCursorPosition(mapWidth + 5, 1);
+                Console.Write("Чтобы приготовить обед. Собери их все!");
+                Console.SetCursorPosition(mapWidth+5, 3);
+                Console.Write("Сумка:");
+                Console.SetCursorPosition(mapWidth + 5, 4);
+                for (int i = 0; i < bag.Length; i++)
+                {
+                    if (i == (windowWidth - mapWidth - 3)/2)
+                    {
+                        Console.SetCursorPosition(mapWidth + 5, 5);
+                    }
+                    if (i == windowWidth - mapWidth - 3)
+                    {
+                        Console.SetCursorPosition(mapWidth + 5, 6);
+                    }
+                    Console.Write(bag[i] + "|");
+                    
+                }
+
+                if (bag.Length == mushroomCounter)
+                {
+
+                    Console.SetCursorPosition(mapWidth + 5, 10);
+                    Console.Write("Поздравляю! Вы собрали все грибы!");
+
+                    Console.SetCursorPosition(mapWidth + 5, 11);
+                    Console.Write("Mожете приготовить из них что-то вкусное!");
+                }
+
+                Console.SetCursorPosition(0, 0);
+
+                for (int row = 0; row < map.GetLength(0); row++)
+                {
+                    for (int col = 0; col < map.GetLength(1); col++)
+                    {
+                        Console.Write(map[row, col]);
+                    }
+                    Console.WriteLine();
+                }
+                if(Y < 1) {
+                    Y++;
+                }
+                if (Y > map.GetLength(1) - 2)
+                {
+                    Y--;
+                }
+                if(X < 1) {
+                    X++;
+                }
+                if(X > map.GetLength(0) - 2)
+                {
+                    X--;
+                }
+                Console.SetCursorPosition(Y, X);
+                Console.Write(mushroomCeeper);
+
+                ConsoleKeyInfo charKey = Console.ReadKey();
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        X--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        X++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Y--;
+                        mushroomCeeper = mushroomCeeperLeft;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Y++;
+                        mushroomCeeper = mushroomCeeperRight;
+                        break;
+                }
+
+                if (map[X, Y] == mushroom)
+                {
+                    map[X, Y] = 'o';
+                    char[] tempBag = new char[bag.Length + 1];
+                    for (int i = 0; i < bag.Length; i++)
+                    {
+                        tempBag[i] = bag[i];
+                    }
+                    tempBag[tempBag.Length - 1] = mushroom;
+                    bag = tempBag;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
